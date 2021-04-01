1ee749c02659bc2e0cd9692eb0148e5754e711b1
Stephen Roberts <stephen.roberts.skybettingandgaming.com>
2021-04-01 16:17:35 +0100

    Cant solve mongo Jacoco error on Message.kt

--- mongodb/build.gradle.kts
+++ mongodb/build.gradle.kts
@@ -78,7 +78,8 @@ tasks {
 			rule {
 				element = "SOURCEFILE"
 				excludes = listOf(
-						"com/example/mongodb/MongodbApplication.kt"
+						"com/example/mongodb/MongodbApplication.kt",
+						"com/example/mongodb/model/Message.kt"
 				)
 				limit {
 
